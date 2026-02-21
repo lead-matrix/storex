e0316be9dadfd28045f799805a478fdf11c092cd
mahmudrb20 <leadmatrix.us@gmail.com>
2026-02-21 17:40:55 +0600

    Fix: Update Supabase project URL and image host

--- next.config.ts
+++ next.config.ts
@@ -11,7 +11,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'gnhsorgofzlevtmuuvtd.supabase.co',
+        hostname: 'qvcxustdtpphagloissl.supabase.co',
       },
     ],
   },
