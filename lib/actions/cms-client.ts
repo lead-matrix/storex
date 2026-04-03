a22382472ede8ac11f5ec8ca4a4a834b493bdae1
lead-matrix <leadmatrix.us@gmail.com>
2026-04-03 21:30:47 +0600

    Create cms-client.ts with server directive and function exports

---
+++ lib/actions/cms-client.ts
@@ -0,0 +1,13 @@
+'use server';
+
+export function ensureAdmin() {
+    // Function implementation
+}
+
+export function createPage() {
+    // Function implementation
+}
+
+export function saveSections() {
+    // Function implementation
+}
